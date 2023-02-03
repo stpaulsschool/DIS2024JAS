46ae4d7e740b074d742e3c6d24679532666e5edf
Jeffrey Scalia <19321@stpauls.qld.edu.au>
2023-02-03 14:01:52 +1000

    Commit

--- Unit1/Capital Finder.py
+++ Unit1/Capital Finder.py
@@ -11,20 +11,25 @@ while Repeat == "yes" or Repeat == "y":
     Phrase = input("What phrase would you like to find the capital letters in? ")
 
     print(" ")
-    print(50 * "=")
+    print(50 * "=")  #Creates a nice break
 
-    for Index, Character in enumerate(Phrase):
-        Index = Index + 1 - NonLetters
-        if Character.isalpha() == False:
-            NonLetters = NonLetters + 1
-        elif Character.isupper() == True:
-            print(f"The capital letter {Character} was found as letter number {Index}")
-            CapitalFound = True
+    for Index, Character in enumerate(Phrase):  #Checks every letter in the phrase, and their index number
+        Index = Index + 1 - NonLetters  #The index will be one behind how a human counts, so 1 is added. Aditionally,
+                                        #some characters are ignored and removed from the total
+        if Character.isalpha() == False:  #Checks for non-letters
+            NonLetters = NonLetters + 1  #Adds to a total that will remove non-letters from the total
+        elif Character.isupper() == True:  #Checks for capitals
+            print(f"The capital letter {Character} was found as letter number {Index}")  #If the letter is capital, it
+                                                                                         #will point out that it is,
+                                                                                         #as well as its location in
+                                                                                         #the phrase
 
-    if CapitalFound == False:
-        print("No capital letters were found.")
+            CapitalFound = True  #Acknowledges that a capital has been found so an error message isn't used
 
-    print(50 * "=")
+    if CapitalFound == False:  #If no capitals are found...
+        print("No capital letters were found.")  #...print an error message
+
+    print(50 * "=") #Creates a nice break
     print(" ")
 
     Repeat = input("Would you like to use this program again? (Yes/No) ").lower()
