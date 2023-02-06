b67f767b440db1cfad1a4803d884b39753d4ebe5
Jeffrey Scalia <19321@stpauls.qld.edu.au>
2023-02-06 12:22:56 +1000

    Commit

--- MAS/Pemutation and Combination Finder.py
+++ MAS/Pemutation and Combination Finder.py
@@ -1,2 +1,16 @@
+inputtotal = 0
+
 print("Welcome to the Permutation and Combination calculator!")
 inputs = input("What characters would you like to find the total permutations and combinations for? ")
+selections = input("How many selections would you like to make? ")
+
+for character in input:
+    if character != " " and character != ",":
+        inputtotal = inputtotal + 1
+
+if selections < inputtotal:
+    print(f"There are not enough characters to make {selections} selections")
+
+if inputtotal < 1 or inputtotal.is_integer() == False or selections < 1 or selections.is_integer() == False:
+   print("Please only insert Natural Numbers")
+else:
---
+++ Unit1/Guessing Game.py
@@ -0,0 +1,18 @@
+import random
+
+print("Welcome to the number guessing game!")
+print(" ")
+print("What difficulty do you want?")
+print("Easy: Unlimited Guesses")
+print("Medium: 10 Guesses")
+print("Hard: 5 Guesses")
+print("Impossible: 1 Guess")
+print(" ")
+difficulty = input("")
+
+number = random.randint(0, 100)
+
+while number != 0:
+    number = random.randit(0, 100)
+
+print(number)
