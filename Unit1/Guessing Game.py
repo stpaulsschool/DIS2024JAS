de2e5f5ea9da6d6740b276b73e47ea827f2b0f24
Jeffrey Scalia <19321@stpauls.qld.edu.au>
2023-02-06 12:47:17 +1000

    Commit

--- Unit1/Guessing Game.py
+++ Unit1/Guessing Game.py
@@ -1,18 +1,43 @@
 import random
 
+NoGuesses = 0
+GuessedOrLost = False
+
 print("Welcome to the number guessing game!")
 print(" ")
 print("What difficulty do you want?")
 print("Easy: Unlimited Guesses")
-print("Medium: 10 Guesses")
+print("Medium: 7 Guesses")
 print("Hard: 5 Guesses")
 print("Impossible: 1 Guess")
 print(" ")
-difficulty = input("")
+Difficulty = input("Difficulty: ").lower()
 
-number = random.randint(0, 100)
+if Difficulty == "easy":
+    Guesses = -1
+elif Difficulty == "medium":
+    Guesses = 7
+elif Difficulty == "hard":
+    Guesses = 5
+elif Difficulty == "impossible":
+    Guesses = 1
 
-while number != 0:
-    number = random.randit(0, 100)
+Number = random.randint(1, 100)
 
-print(number)
+while GuessedOrLost != True:
+    Guess = int(input("What will your guess be? "))
+
+    if Guess > Number:
+        NoGuesses = NoGuesses + 1
+        print("You're guess was too high")
+    elif Guess < Number:
+        NoGuesses = NoGuesses + 1
+        print("You're guess was too low")
+    elif Guess == Number:
+        NoGuesses = NoGuesses + 1
+        print(f"Congrats! You got it in {NoGuesses} guesses!")
+        GuessedOrLost = True
+
+    if GuessedOrLost != True and NoGuesses == Guesses:
+        print(f"You lost. The number was {Number}.")
+        GuessedOrLost = True
