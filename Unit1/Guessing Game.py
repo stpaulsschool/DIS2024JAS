492c1a455373e98653f7184501bf84a5cdd71827
Jeffrey Scalia <19321@stpauls.qld.edu.au>
2023-02-08 09:15:48 +1000

    Commit

--- Unit1/Guessing Game.py
+++ Unit1/Guessing Game.py
@@ -1,43 +1,50 @@
 import random
 
-NoGuesses = 0
-GuessedOrLost = False
+Repeat = "y"
 
 print("Welcome to the number guessing game!")
-print(" ")
-print("What difficulty do you want?")
-print("Easy: Unlimited Guesses")
-print("Medium: 7 Guesses")
-print("Hard: 5 Guesses")
-print("Impossible: 1 Guess")
-print(" ")
-Difficulty = input("Difficulty: ").lower()
 
-if Difficulty == "easy":
-    Guesses = -1
-elif Difficulty == "medium":
-    Guesses = 7
-elif Difficulty == "hard":
-    Guesses = 5
-elif Difficulty == "impossible":
-    Guesses = 1
+while Repeat == "yes" or Repeat == "y":
 
-Number = random.randint(1, 100)
+    GuessedOrLost = False
+    NoGuesses = 0
 
-while GuessedOrLost != True:
-    Guess = int(input("What will your guess be? "))
+    print(" ")
+    print("What difficulty do you want?")
+    print("Easy: Unlimited Guesses")
+    print("Medium: 7 Guesses")
+    print("Hard: 5 Guesses")
+    print("Impossible: 1 Guess")
+    print(" ")
+    Difficulty = input("Difficulty: ").lower()
 
-    if Guess > Number:
-        NoGuesses = NoGuesses + 1
-        print("You're guess was too high")
-    elif Guess < Number:
-        NoGuesses = NoGuesses + 1
-        print("You're guess was too low")
-    elif Guess == Number:
-        NoGuesses = NoGuesses + 1
-        print(f"Congrats! You got it in {NoGuesses} guesses!")
-        GuessedOrLost = True
+    if Difficulty == "easy":
+        Guesses = -1
+    elif Difficulty == "medium":
+        Guesses = 7
+    elif Difficulty == "hard":
+        Guesses = 5
+    elif Difficulty == "impossible":
+        Guesses = 1
 
-    if GuessedOrLost != True and NoGuesses == Guesses:
-        print(f"You lost. The number was {Number}.")
-        GuessedOrLost = True
+    print("Selecting a number between 1 and 100!")
+    Number = random.randint(1, 100)
+
+    while GuessedOrLost == False:
+        Guess = int(input(f"You have {Guesses-NoGuesses} left! What will your guess be? "))
+        if Guess > Number:
+            NoGuesses = NoGuesses + 1
+            print("You're guess was too high")
+        elif Guess < Number:
+            NoGuesses = NoGuesses + 1
+            print("You're guess was too low")
+        elif Guess == Number:
+            NoGuesses = NoGuesses + 1
+            print(f"Congrats! You got it in {NoGuesses} guesses!")
+            GuessedOrLost = True
+
+        if GuessedOrLost != True and NoGuesses == Guesses:
+            print(f"You lost. The number was {Number}.")
+            GuessedOrLost = True
+
+Repeat = input("Would you like to play again? (Yes/No) ").lower()
